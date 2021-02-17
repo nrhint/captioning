d4b04b311e0f45bae0cf1b97bddd7551eb04c8e1
nrhint <nathan@hintonclan.org>
2021-02-16 22:23:32 -0800

    worked on the manual script. The input is not working.

--- captionFromURL.py
+++ captionFromURL.py
@@ -5,10 +5,12 @@ from util.verse_util import get_verse_from_file
 from util.video_util import get_time_from_video
 from util.subtitle_util import generate_srt, generate_srt_adv
 
+book = 'D&C'
+
 urlList = open('resources/book/capture.csv', 'r').read()
 urlList = urlList.split('\n') #The urlList file is a set of new line spaced links with the first one being a comment
 urlList.pop(0) #The first line is a comment that says the purpuse of the file # Header
-https://www.churchofjesuschrist.org/study/scriptures/dc-testament/dc/1?lang=ase
+BaseUrl = 'https://www.churchofjesuschrist.org/study/scriptures/dc-testament/dc/1?lang=ase'
 
 for detail in urlList:
     details = detail.split(',')
