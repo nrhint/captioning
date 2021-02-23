7925178869c6b12925816629753a0b329b066602
Amornchai Amornkulpittaya <Arashage@gmail.com>
2021-02-24 05:12:42 +0700

    Skip download faild link

--- util/download.py
+++ util/download.py
@@ -9,4 +9,3 @@ def download_video(url, file_path, file_name, file_extension):
             urllib.request.urlretrieve(url, '%s/%s.%s'%(file_path, file_name, file_extension))
     except:
         print('Cannot download file "%s/%s.%s".'%(file_path, file_name, file_extension))
-        raise Exception
