a3e5ae26674c38f917e99a73c6eba7c2a5a32447
Amornchai Amornkulpittaya <Amornchai-A@hotmail.com>
2021-03-18 13:11:01 +0700

    Make m01 log same as print

--- m01generateVerseAndVideoURL.py
+++ m01generateVerseAndVideoURL.py
@@ -8,7 +8,7 @@ from util.logging_util import add_to_log, save_log
 
 log = ''
 print('\n-- Start Processing -- ')
-log = add_to_log(log, '\n-- Start Processing -- ')
+log = add_to_log(log, '\n-- Start Processing -- \n')
 csv = read_file('Resources', 'Book List', 'csv')
 csv = remove_space_delimeter(csv, ',')
 book_list = csv.split('\n') #The urlList file is a set of new line spaced links with the first one being a comment
@@ -24,9 +24,13 @@ for book_csv in book_list:
     book_detail = book_csv.split(',')
     book = Book(book_detail[0], book_detail[1], book_detail[2], int(book_detail[3]), int(book_detail[4]), int(book_detail[5]))
 
-    print('\nStart on Book %s'%(book.video_prefix))
-    log = add_to_log(log, '\nStart on Book %s'%(book.video_prefix))
-    missing_list += '\t%s:'%(book.video_prefix)
+    book_name = book.video_prefix
+    if book_name == '':
+        book_name = book.book_name
+    print('\nStart on Book %s'%(book_name))
+    log = add_to_log(log, '\nStart on Book %s\n'%(book_name))
+    missing_list += '\t%s:\t'%(book_name)
+    detail_temp = ''
     video_list = 'Chapter,\tLink'
     for chapter_number in range(1, book.max_chapter + 1):
         video_url = read_from_website(book, chapter_number, manual_url)
@@ -34,20 +38,24 @@ for book_csv in book_list:
         if video_url == '-- Need Video URL --':
             print('\t(M)', end = '')
             log = add_to_log(log, '\t(M)')
-            missing_list += '\t%s %s'%(book.video_prefix, chapter_number)
+            detail_temp += '\t%s %s'%(book_name, chapter_number)
         else:
             print('\t%s %s'%(book.video_prefix, chapter_number), end ='')
-            log = add_to_log(log, '\t%s %s'%(book.video_prefix, chapter_number))
+            log = add_to_log(log, '\t%s %s'%(book_name, chapter_number))
         if chapter_number % 10 == 0:
             print('')
-            log = add_to_log(log, '')
-        save_log(log, file_name = 'generateVerseAndText')
-    write_file('Resources/Video URL', book.video_prefix, 'csv', video_list)
+            log = add_to_log(log, '\n')
+        save_log(log, file_name = 'm01_generateVerseAndText')
+    if detail_temp != '':
+        missing_list += detail_temp + '\n'
+    else:
+        missing_list += "Pass\n" 
+    save_log(missing_list, file_name = 'm01_missing')
+    write_file('Resources/Video URL', book_name, 'csv', video_list)
     print('\nFinish Book')
-    log = add_to_log(log, '\nFinish Book')
-    missing_list += '\n'
+    log = add_to_log(log, '\nFinish Book\n')
+    save_log(log, file_name = 'm01_generateVerseAndText')
 
-if missing_list != '\nMissing Link\n':
-    print(missing_list, end = '')
-save_log(missing_list, file_name = 'missing')
+print(missing_list, end = '')
+save_log(missing_list, file_name = 'm01_missing')
 input()
--- util/logging_util.py
+++ util/logging_util.py
@@ -1,9 +1,11 @@
 #This will add a logging functions to the program
+from datetime import datetime
 from util.file_util import write_file
 from time import time
 
-def save_log(log, file_path = '.', file_name = 'log'):
-    write_file(file_path, file_name, 'log', log)
+def save_log(log, file_path = 'log', file_name = 'log'):
+    current = datetime.now().strftime('%Y-%m-%d')# %H:%M:%S')
+    write_file(file_path, file_name + '_' + str(current), 'log', log)
 
 def add_to_log(log, data):
-    return log+'\n%s'%str(data)
+    return log+'%s'%str(data)
